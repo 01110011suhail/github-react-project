ea5ba713ae2e9d0f3e2153d2b4621755e752b83c
01110011suhail <sha433415@gmail.com>
2024-08-29 16:06:59 +0530

    first

--- countdowntime/src/App.js
+++ countdowntime/src/App.js
@@ -89,6 +89,7 @@ useEffect(()=>{
 console.log(hours,minutes,seconds)
 
   return (
+    <div>
     <div className='app'>
       <h2>Count Down TIme</h2>
       {
@@ -129,6 +130,49 @@ console.log(hours,minutes,seconds)
 
      }
       
+    </div>
+
+    
+<div class="wrapper">
+  <div class="candles">
+    <div class="light__wave"></div>
+    <div class="candle1">
+      <div class="candle1__body">
+        <div class="candle1__eyes">
+          <span class="candle1__eyes-one"></span>
+          <span class="candle1__eyes-two"></span>
+        </div>
+        <div class="candle1__mouth"></div>
+      </div>
+      <div class="candle1__stick"></div>
+    </div>
+    
+    <div class="candle2">
+      <div class="candle2__body">
+        <div class="candle2__eyes">
+          <div class="candle2__eyes-one"></div>
+          <div class="candle2__eyes-two"></div>
+        </div>
+      </div>
+      <div class="candle2__stick"></div>
+    </div>
+    <div class="candle2__fire"></div>
+    <div class="sparkles-one"></div>
+    <div class="sparkles-two"></div>
+    <div class="candle__smoke-one">
+
+    </div>
+    <div class="candle__smoke-two">
+
+    </div>
+    
+  </div>
+  <div class="floor">
+  </div>
+  
+
+</div>
+  
     </div>
   )
 }
